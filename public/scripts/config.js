f594e333e9122550f732d3c5a6424c345bac7e7a
elisa lee <elisa.my.lee@gmail.com>
2016-10-31 16:04:40 -0500

    set up authentication

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,15 +1,15 @@
 app.config(function ($routeProvider, $locationProvider) {
-  $routeProvider.when('/plant', {
+  $routeProvider.when('/user/plant', {
             templateUrl: 'views/plant.html',
-            controller: 'PlantController as plantCtrl',
-          }).when('/grow', {
+            controller: 'PlantController as pCtrl',
+          }).when('/user/grow', {
             templateUrl: 'views/grow.html',
-            controller: 'GrowController as growCtrl',
-          }).when('/garden', {
+            controller: 'GrowController as gCtrl',
+          }).when('/user/garden', {
             templateUrl: 'views/garden.html',
-            controller: 'GardenController as gardenCtrl'
-          // }).otherwise({
-          //   redirectTo: '/plant',
+            controller: 'GardenController as gCtrl',
+          }).otherwise({
+            redirectTo: '/login',
           });
 
   $locationProvider.html5Mode(true);
