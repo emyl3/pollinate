357a82640176c396d91c16d21bc08508fc8e1e4f
elisa lee <elisa.my.lee@gmail.com>
2016-11-04 16:01:19 -0500

    add form to send text

--- public/scripts/controllers/sendFlowerModal.controller.js
+++ public/scripts/controllers/sendFlowerModal.controller.js
@@ -3,6 +3,10 @@ app.controller('SendFlowerModalController', SendFlowerModalController);
 function SendFlowerModalController($uibModalInstance, flower, flowerId) {
   var ctrl = this;
 
+  ctrl.submitForm = function (phone, message) {
+    console.log(phone);
+    console.log(message);
+  }
   // flower.getReward(flowerId).then(function (response) {
   //   ctrl.url = response[0].url;
   // });
