94efca3b2f07668d1180225a58a388c172fdf7e7
elisa lee <elisa.my.lee@gmail.com>
2016-11-08 10:46:58 -0600

    add phone number

--- routes/twilioroute.js
+++ routes/twilioroute.js
@@ -1,26 +1,40 @@
 const router = require('express').Router();
 const dotenv = require('dotenv');
-dotenv.load();
-
 const twilio = require('twilio');
+
+dotenv.load();
 
 const TWILIO_ACC = process.env.TWILIO_ACC_SID;
 const TWILIO_AUTH = process.env.TWILIO_AUTH_TOKEN;
+const TWILIO_NUMBER = process.env.TWILIO_NUMBER;
 var client = twilio(TWILIO_ACC, TWILIO_AUTH);
 
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
   var phone = req.body.phone;
   var message = req.body.message;
   var url = req.body.flowerUrl;
-  console.log(url);
-  console.log('posting');
 
-  client.sendMessage({
-    to: '+1' + phone,
-    from: '+12012920629',
-    body: message + ' Replies to this number will not work.',
-    MediaUrl: url,
-  });
+  // client.outgoingCallerIds.create({
+  //   phoneNumber: '+1' + phone,
+  // }, function (err, callerId) {
+  //   process.stdout.write(callerId.sid);
+  //   return
+  // });
+  //
+  // promise.then(function (response) {
+  //   console.log('success adding', response);
+  // }, function (error) {
+  //
+  //   console.log('failure adding,', error);
+  // });
+      client.sendMessage({
+        to: '+1' + phone,
+        from: '+' + TWILIO_NUMBER,
+        body: message + ' Replies to this number will not work.',
+        MediaUrl: url,
+      });
+
+
   res.sendStatus(200);
 });
 
