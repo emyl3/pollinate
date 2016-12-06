9e54eaa4c93dad937e879640ed3322f374324897
elisa lee <elisa.my.lee@gmail.com>
2016-12-05 21:03:28 -0600

    comment on determineHeader function

--- public/scripts/controllers/nav.controller.js
+++ public/scripts/controllers/nav.controller.js
@@ -3,7 +3,7 @@ app.controller('NavController', NavController);
 function NavController($http, $location) {
   var ctrl = this;
   var location = $location.url();
-  determineHeader();
+  determineHeader(); //edit
 
   ctrl.logout = function () {
     $http.post('/logout', {}).then(function (response) {
@@ -14,6 +14,8 @@ function NavController($http, $location) {
       }
     });
   };
+
+  //needs to be edited to include other header options
 
   function determineHeader() {
     console.log(location);
