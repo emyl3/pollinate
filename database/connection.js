306d909e962cb5d887aa7135e95ab11229c1f527
elisa lee <elisa.my.lee@gmail.com>
2016-12-07 07:12:26 -0600

    heroku setup

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,29 @@
-const pg = require('pg');
+// const pg = require('pg');
+//
+// var config = {
+//   database: 'pollinate',
+// };
+//
+// var pool = new pg.Pool(config);
+//
+// module.exports = pool;
 
-var config = {
-  database: 'pollinate',
+const url = require('url');
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+// DB setup for Knex.js
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: true,
+    },
+  },
 };
-
-var pool = new pg.Pool(config);
-
-module.exports = pool;
