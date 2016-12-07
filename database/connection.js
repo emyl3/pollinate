1579a7236333df69321cd0c797eafd4b4dbbd5fb
elisa lee <elisa.my.lee@gmail.com>
2016-12-07 07:21:48 -0600

    setup for localhost

--- database/connection.js
+++ database/connection.js
@@ -1,29 +1,29 @@
-// const pg = require('pg');
-//
-// var config = {
-//   database: 'pollinate',
-// };
-//
-// var pool = new pg.Pool(config);
-//
-// module.exports = pool;
+const pg = require('pg');
 
-const url = require('url');
-
-const params = url.parse(process.env.DATABASE_URL);
-const auth = params.auth.split(':');
-
-// DB setup for Knex.js
-module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      user: auth[0],
-      password: auth[1],
-      host: params.hostname,
-      port: params.port,
-      database: params.pathname.split('/')[1],
-      ssl: true,
-    },
-  },
+var config = {
+  database: 'pollinate',
 };
+
+var pool = new pg.Pool(config);
+
+module.exports = pool;
+
+// const url = require('url');
+//
+// const params = url.parse(process.env.DATABASE_URL);
+// const auth = params.auth.split(':');
+//
+// // DB setup for Knex.js
+// module.exports = {
+//   development: {
+//     client: 'pg',
+//     connection: {
+//       user: auth[0],
+//       password: auth[1],
+//       host: params.hostname,
+//       port: params.port,
+//       database: params.pathname.split('/')[1],
+//       ssl: true,
+//     },
+//   },
+// };
